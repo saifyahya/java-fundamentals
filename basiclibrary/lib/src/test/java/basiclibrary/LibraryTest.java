6a15630a0f0a04fafbfb5b944a2ec4c7f2e2cf57
saifyahya <saifbarakat991@gmail.com>
2023-08-13 16:14:46 +0000

    linter+2methods basiclibrary

--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -3,12 +3,57 @@
  */
 package basiclibrary;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
+/*
         assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+*/
+    }
+    @Test public void testRoll() {
+        Library sut = new Library();
+        int result[] = sut.roll(3);
+        Assertions.assertEquals(3, result.length);
+    }
+    @Test public void testAverage() {
+        Library sut = new Library();
+        double result = sut.average(new int[]{3, 1, 2});
+        Assertions.assertEquals(2.0, result);
+    }
+    @Test public void testContainsDuplicates() {
+        Library sut = new Library();
+        boolean result =  sut.containsDuplicates(new int[]{});
+        Assertions.assertEquals(false,result);
+    }
+    @Test public void testLowestAverageArray() {
+        Library sut = new Library();
+        int result[] = sut.lowestAverageArray(new int[][] {{1,2,3},{4,5,6},{1}});
+        int expectedResult[]={1};
+        Assertions.assertEquals(Arrays.toString(expectedResult),Arrays.toString(result));
+    }
+
+    @Test public void testTally() {
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+        Library sut = new Library();
+        String result = sut.tally(votes);
+        Assertions.assertEquals("Bush",result);
     }
 }
